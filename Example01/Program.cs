9129871fb94cbd27207c95ddce24c5d52ec2d8aa
Andrei Shvetsov <shved963@gmail.com>
2022-12-14 22:55:04 +0300

    Seminar7Lesson Example01 and Common libs

---
+++ Example01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте двумерный массив размером m*n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+
+using static Common.Helper;
+
+Console.Clear();
+
+int [,] array = Common.Helper.CreateRandom2DArray(3, 4);
+Common.Helper.PrintArray(array);

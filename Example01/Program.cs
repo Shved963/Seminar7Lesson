f98276bbcd3b052e0f6bcf208e8f144537360259
Andrei Shvetsov <shved963@gmail.com>
2022-12-14 23:26:53 +0300

    info about String added

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -3,8 +3,36 @@
 // m = 3, n = 4.
 
 using static Common.Helper;
-
-Console.Clear();
+//2D Array
+/*Console.Clear();
 
 int [,] array = Common.Helper.CreateRandom2DArray(3, 4);
-Common.Helper.PrintArray(array);
+Common.Helper.PrintArray(array);*/
+
+
+// Tuples
+/*
+(int, int) coordinates = GetSomthing();
+Console.WriteLine(coordinates.Item1);
+Console.WriteLine(coordinates.Item2);
+
+(int, int) GetSomthing()
+{
+    int a = 5;
+    int b = 10;
+    return(a, b);
+}
+*/
+
+int [] array = new int[3]{1,3,4};
+
+string str = string.Join(", ",array);
+Console.WriteLine(str);
+
+string strFormat = string.Format("это лзааы{0} равно аыавы {1} dsdsdad {2}", 1, 2, 3);
+
+string str2 = $"{str}";
+
+string.Concat("", "", "");
+//string.Empty(); //создание пустой строки
+string.IsNullOrWhiteSpace();

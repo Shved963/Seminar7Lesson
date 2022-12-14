f7a1bd4c1c4ebb473b565d616403e81bf250382f
Andrei Shvetsov <shved963@gmail.com>
2022-12-14 23:54:48 +0300

    Example02 added

--- Example02/Program.cs
+++ Example02/Program.cs
@@ -7,25 +7,25 @@
 // 2 3 4 5
 using static Common.Helper;
 Console.Clear();
+Console.WriteLine("Введите размерность двумерного массива");
+int[,] array = Create2DArray();
+
+Common.Helper.PrintArray(array);
 
 
 
-
-
-
-int[,] CreateRandom2DArray()
+int[,] Create2DArray()
 {
     int countOfRow = Common.Helper.IntoInt();
     int countOfColums = Common.Helper.IntoInt();
     
     int[,] array = new int[countOfRow, countOfColums];
-    Random random = new Random();
 
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = random.Next(-10, 11);
+            array[i, j] = i + j;
         }
     }
 
